1391fa8f40ffc60a57b1e381f20bdb803ca6121c
skm5786 <sumitthakral3@gmail.com>
2022-06-24 22:53:55 +0530

    Initial Commit

---
+++ C++ programming/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ programming
+//
+//  Created by sumit thakral on 24/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
